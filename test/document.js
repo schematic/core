5afbf42f7145ca4f74d98bf81d3613be12fda66f
Chris Tarquini <chris@tarq.io>
2013-11-22 20:50:49 -0500

    Added unit tests, added support for documents

---
+++ test/document.js
@@ -0,0 +1,39 @@
+/* global describe, it ,before */
+var assert = require('chai').assert
+  , TypeContainer = require('../lib/type-container')
+  , DocumentType = require('../types/document')
+  , StringType = require('../types/string')
+
+describe.only('Document Type', function() {
+  var tc;
+  before(function() {
+    tc = TypeContainer()
+    tc('Document', DocumentType)
+    DocumentType.type('String', StringType)
+  })
+
+  it('should use own type container', function() {
+    tc('Test', StringType)
+    try {
+      var type = tc.infer({foo: {type: 'Test'}})
+      assert.notInstanceOf(type.tree.foo, StringType, 'local type container')
+    } catch (err) {
+      assert(true, 'local type container')
+    }
+  })
+
+  describe('Type Inference', function() {
+    it('should infer schema', function() {
+      var type = tc.infer({foo: String})
+      assert.instanceOf(type, DocumentType, 'from document literal')
+      assert.instanceOf(type.tree.foo, StringType, 'item type')
+    })
+
+    it('should ignore explicit types with a `type` property', function(){
+      var type = tc.infer({type: {type: String}})
+      assert.instanceOf(type.tree.type, StringType)
+    })
+
+
+  })
+})
