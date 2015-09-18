e488754b7d7ab5bd5296c9402fdad2eb749d29f3
Christopher Tarquini <chris+github@tarq.io>
2015-09-18 11:10:43 -0400

    Added boolean middleware

--- types/boolean.js
+++ types/boolean.js
@@ -6,9 +6,21 @@ Schema
   .check(check)
   .cast(cast);
 
+
+exports.plugin = function() {
+  return function (types) {
+    types.on('infer', middleware);
+  }
+}
+
+function middleware(info) {
+  if (info.type === Boolean)
+    info.type = exports;
+}
 function check(value) {
   return value === true || value === false;
 }
+
 
 function cast(value) {
   if (null === value || undefined === value) return value;
